f8dda07e970113ec5402c3d0b43ddb254c6145dd
Luc Bourlier <luc.bourlier@typesafe.com>
2012-05-03 11:28:49 +0200

    Added asynchronous fetching (status unknown)

--- g8_bashcompletion/g8.sh
+++ g8_bashcompletion/g8.sh
@@ -10,10 +10,20 @@ _g8()
 	  return 0
 	fi
 
+	( _g8_fetch & )
 
-        opts=$(g8 --list | awk '{print $1}')
+	opts="test test2"
 
 	COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+
+	echo $COMPREPLY > /tmp/g8_bashcompletion_result
+
+        COMPREPLY=( "g8 completion caching in progress." "please wait." )
+}
+
+_g8_fetch()
+{
+	g8 --list | awk '{print $1}' > /tmp/g8_bashcompletion_list_cache &
 }
 
 complete -F _g8 g8
